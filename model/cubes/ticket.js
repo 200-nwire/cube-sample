5ca541ff39b4271bf0a8421be42ff8da7b6e20c6
nikolay <nikolay@200apps.co>
2025-05-08 12:21:46 +0000

    Merge nikolay/slaCounter to master

--- model/cubes/ticket.js
+++ model/cubes/ticket.js
@@ -86,12 +86,7 @@ cube(`ticket`, {
       sql: `${CUBE}."updatedAt"`,
       type: `time`
     },
-
-    countSlaBreached: {
-      sql: `CASE WHEN ${CUBE}."slaBreached" = true THEN 1 ELSE NULL END`,
-      type: `number`
-    },
-
+    
   },
   
   measures: {
