86614dcbcd84f37c0096570cc6721be59e030edd
avinoam <avinoam@200apps.co>
2025-08-05 08:18:39 +0000

    Merge avinoam/remove-companies to master

--- cube.js
+++ cube.js
@@ -4,31 +4,36 @@
 // CubeCloud users by default.  Please contact support if you need them
 // enabled for your account.  You are still allowed to require
 // @cubejs-backend/*-driver packages.
+
 const excludedCompanyIds = ["bcn00kc2q66j", "h1wgedy43j4e"];
 
 module.exports = {
   queryRewrite: (query, { securityContext }) => {
-    if (securityContext.companyIds) {
-      const cubeNames = [
-        ...(query.dimensions || []),
-        ...(query.measures || []),
-      ].map((e) => e.split(".")[0]);
+    const cubeNames = [
+      ...(query.dimensions || []),
+      ...(query.measures || []),
+    ].map((e) => e.split(".")[0]);
 
+    if (securityContext.companyIds) {
       cubeNames.forEach(cube => {
         query.filters.push({
           member: `${cube}.companyId`,
-          operator: 'in',
+          operator: 'equals',
           values: securityContext.companyIds,
         });
-        query.filters.push({
-          member: `${cube}.companyId`,
-          operator: 'notEquals',
-          values: excludedCompanyIds,
-        });
-      })
+      });
     }
 
-    return query;   
+    cubeNames.forEach(cube => {
+      query.filters.push({
+        member: `${cube}.companyId`,
+        operator: 'notEquals',
+        values: excludedCompanyIds,
+      });
+    });
+
+    return query;
   },
 };
+
 
